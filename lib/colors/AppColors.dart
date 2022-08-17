385ec8c100da51c62e01503070e02f9f07e21e86
Muckesh <muckesh.k.s@gmail.com>
2022-08-17 06:16:08 +0530

    setting up

---
+++ lib/colors/AppColors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff675D5D);
+  static const secondayColor = Color(0xff52CFF1);
+  static const black = Color(0xff000000);
+  static const white = Color(0xffFFFFFF);
+  static const containerColor = Color(0xffB8B4B4);
+  static const btnColor = Color(0xff30C4EC);
+  static const priceColor = Color(0xff69F68A);
+}
